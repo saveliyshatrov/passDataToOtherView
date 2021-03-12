8d99aa8e419869e3f4de2f3fc2c16e08f6e926d7
time_guy <31863650+saveliyshatrov@users.noreply.github.com>
2021-03-12 16:36:12 +0300

    Initial Commit

---
+++ passDataToOtherView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  passDataToOtherView
+//
+//  Created by Савелий Шатров on 12.03.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ passDataToOtherView/passDataToOtherViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  passDataToOtherViewApp.swift
+//  passDataToOtherView
+//
+//  Created by Савелий Шатров on 12.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct passDataToOtherViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
