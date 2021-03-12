5b4772ebc5d5b9695fc408fb29996829973a1472
time_guy <31863650+saveliyshatrov@users.noreply.github.com>
2021-03-12 20:20:34 +0300

    Update ContentView.swift

--- passDataToOtherView/ContentView.swift
+++ passDataToOtherView/ContentView.swift
@@ -8,14 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var text: String = ""
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack{
+                TextField("Enter text", text: $text)
+                    .textFieldStyle(PlainTextFieldStyle())
+                    .navigationBarTitleDisplayMode(.inline)
+                    .frame(width: 300, height: 490, alignment: .topLeading)
+            }
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    VStack {
+                        Text("First view").font(.headline)
+                    }
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    NavigationLink(destination: SecondView(text: self.text)) {
+                                        Text("Done")
+                    }.navigationTitle("Second view")
+                }
+            }
+        }
+    }
+}
+
+struct SecondView: View {
+    var text: String = ""
+    var body: some View {
+        Text(self.text).toolbar {
+            ToolbarItem(placement: .principal) {
+                VStack {
+                    Text("Second view").font(.headline)
+                }
+            }
+        }.frame(width: 300, height: 490, alignment: .topLeading)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
     }
 }
